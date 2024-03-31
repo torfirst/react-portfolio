c51319811811cebff80430c81431cb51ded519a6
tor first <victoriah.first@gmail.com>
2024-03-31 00:37:51 -0500

    removed extraneous files

--- src/components/PDF-Viewer.jsx
+++
@@ -1,8 +0,0 @@
-import { Document, Page } from '@react-pdf/renderer';
-import Resume from '../assets/torfirst_resume.pdf';
-
-export default function PDFViewer() {
-  <Document file={Resume}>
-    <Page pageNumber={1} />
-  </Document>
-}
